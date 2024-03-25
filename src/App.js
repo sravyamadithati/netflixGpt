003bb765289e69e65378f936b89f40bd30edde90
Sravya <madithatisravya1998@gmail.com>
2024-03-25 10:32:32 +0530

    Removed unused file imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux";
 import Store from "./utils/appStore";
 import Body from "./components/Body";
-import DummyTest from "./DummyTest";
 function App() {
   return (
     <Provider store={Store}>
